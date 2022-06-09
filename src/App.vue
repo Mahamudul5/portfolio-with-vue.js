9019cff7ef2b9fa507575f1e9bb0fe7361e7d9f4
mahamudul5 <mh.bilash22@gmail.com>
2022-06-09 16:50:59 +0600

    sectionOne complete

--- src/App.vue
+++ src/App.vue
@@ -19,9 +19,35 @@
         <v-row class="align-center px-10" style="height:690px">
           <v-col lg="6" cols="6" class="pa-6 font-weight-bold py-16">
             <h2 class="display-3 green--text">Frontend Web Developer.</h2>
-
+            <v-subheader>I am Mahamudul hasan. I am a frontend developer ,work with react.js, vue.js ,vuetify ,tailwind
+              etc.</v-subheader>
+            <br />
+            <br />
+            <v-btn large elevation="5" color="green" class="white--text mt-10 px-10 py-5">Hire me</v-btn>
+            <v-btn large elevation="5" color="green" class="white--text mt-10 ml-10 px-10 py-5">contact me</v-btn>
+            <v-row class="mt-16">
+              <v-chip class="px-4 ma-3" color="success">
+                <v-icon left size="20">mdi-whatsapp</v-icon>
+                what's app
+              </v-chip>
+              <v-chip class="px-4 ma-3" color="success">
+                <v-icon left size="20">mdi-github</v-icon>
+                github
+              </v-chip>
+              <v-chip class="px-4 ma-3" color="success">
+                <v-icon left size="20">mdi-linkedin</v-icon>
+                linkedIn
+              </v-chip>
+              <v-chip class="px-4 ma-3" color="success">
+                <v-icon left size="20">mdi-facebook</v-icon>
+                facebook
+              </v-chip>
+            </v-row>
           </v-col>
+          <v-col lg="6" cols="6">
 
+            <v-img src="../public/images/myProfileR.png"></v-img>
+          </v-col>
         </v-row>
 
       </v-section>
